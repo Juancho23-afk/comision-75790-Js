2a4a174eb00c89feaf42ce3c48e5790cf51b0723
Juan Olmedo <jn0044501@gmail.com>
2025-05-03 19:13:48 -0300

    empiezo a trtabajar con el json en js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const containerCartProducts = document.querySelector('.container-cart-products')
 const totalPagar = document.querySelector('.total-pagar');
 const contadorProductos = document.getElementById('contador-productos');
 const mensajeVacio = document.querySelector('.cart-empty-message');
+const continerItems = document.querySelector('.container-item');
 
 let contador = 0;
 let totalActual = 0;
@@ -13,73 +14,8 @@ btnCart.addEventListener('click', function () {
 });
 
 
-const botonesAgregar = document.querySelectorAll('.btn-add-cart');
-for (let i = 0; i < botonesAgregar.length; i++) {
-    botonesAgregar[i].addEventListener('click', function (e) {
-        const producto = e.target.closest('.items');
-        const tituloProducto = producto.querySelector('h2').textContent;
-        const precioProducto = producto.querySelector('.price').textContent;
+/*funcion para cargar los productos desde el Json */
 
-        // Creo el nuevo elemento que representa el producto en el carrito
-        const nuevoElemento = document.createElement('div');
-        nuevoElemento.classList.add('cart-product');
-        nuevoElemento.innerHTML = `
-            <div class="info-cart-product">
-                <span class="cantidad-producto-carrito"></span>
-                <p class="titulo-producto-carrito">${tituloProducto}</p>
-                <span class="precio-producto-carrito">${precioProducto}</span>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                stroke-width="1.5" stroke="currentColor" class="icon-close">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-        `;
-        
-        const totalDiv = containerCartProducts.querySelector('.cart-total');
-        containerCartProducts.insertBefore(nuevoElemento, totalDiv);
-
-        
-        contador++;
-        contadorProductos.textContent = contador;
-
-        // Se suma el producto nuevo al total
-        const precioSoloNumero = parseInt(precioProducto.replace('$', '').replace(',', ''));
-        totalActual += precioSoloNumero;
-        totalPagar.textContent = `$${totalActual.toLocaleString()}`;
-
-        //agrego el alert de sweet alert
-        Swal.fire({
-            title: '¡Producto añadido!',
-            text: `${tituloProducto} ha sido añadido al carrito.`,
-            icon: 'success',
-            confirmButtonText: 'OK'
-        });
-
-        // Agrego eliminar productode carrito
-        const botonEliminar = nuevoElemento.querySelector('.icon-close');
-        botonEliminar.addEventListener('click', function () {
-            nuevoElemento.remove();
-            contador--;
-            contadorProductos.textContent = contador;
-            totalActual -= precioSoloNumero;
-
-            // Actualizo el total si el carrito está vacio pongo 0
-            if (contador === 0) {
-                totalPagar.textContent = "$0";
-            } else {
-                totalPagar.textContent = `$${totalActual.toLocaleString()}`;
-            }
-
-            // Guardo el estado del carrito
-            guardarCarritoEnLocalStorage();
-            actualizarMensajeCarritoVacio();
-        });
-
-        // Guardo los cambios en localStorage
-        guardarCarritoEnLocalStorage();
-        actualizarMensajeCarritoVacio();
-    });
-}
 
 document.addEventListener('DOMContentLoaded', function () {
     // Recupero los datos del carrito guardados en localStorage
